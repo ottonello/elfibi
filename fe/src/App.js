20c76d803259a51249c1a5b86907a2905ab055c5
marcos <marcos@tiqets.com>
2020-10-26 20:21:52 +0100

    Add date picker

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './App.css';
 import Plot from 'react-plotly.js';
 import Button from 'react-bootstrap/Button';
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 class App extends React.Component {
+
     constructor(props) {
         super(props)
         this.state = {
@@ -15,7 +19,8 @@ class App extends React.Component {
                     mode: 'lines+markers',
                     marker: {color: 'red'},
                 }
-            ]
+            ],
+            startDate: new Date()
         }
         console.log("YES")
     }
@@ -42,7 +47,7 @@ class App extends React.Component {
                 this.setState({data: new_data})
             })
             .catch(function (error) {
-                console.log(error)
+                    console.log(error)
                     window.location = "http://localhost:8080/auth";
                 }
             );
@@ -56,8 +61,13 @@ class App extends React.Component {
                     layout={{title: 'Your heart rate'}}
                     style={{width: "100%", height: "100%"}}
                 />
-                <Button variant="primary" onClick={(e) => this.handleChange(e)}>Fetch those heart beats</Button>{' '}
 
+                <Button variant="primary" onClick={(e) => this.handleChange(e)}>Fetch those heart beats</Button>{' '}
+                <DatePicker
+                    selected={this.state.startDate}
+                    onChange={date => this.setState({startDate: date})}
+                />
+                <p>{this.state.startDate.toString()}</p>
             </div>
         );
     }
